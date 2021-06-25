1ea733dbb6223bf9deb0f2652caba2b07f99a9fa
Chris Phillips <C_L_Phillips72@hotmail.com>
2021-06-24 19:31:10 -0500

    Adding to the Single Page App

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,30 +1,45 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
 import { CAMPSITES } from '../shared/campsites';
+import { COMMENTS } from '../shared/comments';
+import { PARTNERS } from '../shared/partners';
+import { PROMOTIONS } from '../shared/promotions';
 import CampsiteInfo from './CampsiteInfoComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
+
 
 class Main extends Component {
     constructor(props){
         super(props);
-        this.state = { campsites: CAMPSITES };
+        this.state = { campsites:  CAMPSITES,
+                       comments:   COMMENTS,
+                       partners:   PARTNERS,
+                       promotions: PROMOTIONS
+                     }
     }
 
     render() {
         const HomePage = () => {
             return (
-                <Home />
+                <Home 
+                    campsite={this.state.campsites.filter(campsite => campsite.featured)[0]} 
+                    promotion={this.state.promotions.filter(promotion => promotion.featured)[0]} 
+                    partner={this.state.partners.filter(partner => partner.featured)[0]} 
+                />
             );
         }
         return (
             <div>
                 <Header />   
                 <Switch> 
+                    {/* use component={} when component doesn't require state data, otherwise use render={} */}
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={ this.state.campsites } />} />
+                    <Route exact path='/contactus' component={Contact} /> 
                 </Switch>
                 <Footer />
             </div>
