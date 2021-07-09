a6f79b7afd013b4badd48d3cefe10597d8aa4889
Chris Phillips <C_L_Phillips72@hotmail.com>
2021-07-08 21:27:04 -0500

    Fetch from Server

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,5 +1,5 @@
 import * as ActionTypes from './ActionTypes';
-import { CAMPSITES } from '../shared/campsites';
+import { baseUrl } from '../shared/baseUrl';
 
 export const addComment = (campsiteId, rating, author, text) => ({
     type: ActionTypes.ADD_COMMENT,
@@ -14,10 +14,22 @@ export const addComment = (campsiteId, rating, author, text) => ({
 export const fetchCampsites = () => dispatch => {
 
     dispatch(campsitesLoading());
-
-    setTimeout(() => {
-        dispatch(addCampsites(CAMPSITES));
-    }, 2000);
+    
+    return fetch(baseUrl + 'campsites')
+    // calling fetch() and returning the result.  baseUrl is an imported file
+    // and campsite is the location for the resource we want.  A call to fetch returns 
+    // a promise.
+      .then(response => response.json())  
+    //When that promise is resolved, the then() method will use the
+    // response.json() method to convert the response from json to javascript, and that 
+    // javascript will be an array of campsites.  The json method returns a new 
+    // promise for which the converted javascript array is the new response value when 
+    // it resolves. 
+      .then(campsites => dispatch(addCampsites(campsites)))
+     // grabbing that javascript array is the first campsites argument once that promise
+     // resolves.  Then we can dispatch that campsites argument with the addCampsites
+     // action creator to be used as its payload.  we won't be dealeing with errors
+     // or adding a catch() mehtod to this fetch() yet but will be coming.
 };
 
 export const campsitesLoading = () => ({
@@ -32,4 +44,42 @@ export const campsitesFailed = errMess => ({
 export const addCampsites = campsites => ({
     type: ActionTypes.ADD_CAMPSITES,
     payload: campsites
+});
+
+export const fetchComments = () => dispatch => {    
+    return fetch(baseUrl + 'comments')
+        .then(response => response.json())
+        .then(comments => dispatch(addComments(comments)));
+};
+
+export const commentsFailed = errMess => ({
+    type: ActionTypes.COMMENTS_FAILED,
+    payload: errMess
+});
+
+export const addComments = comments => ({
+    type: ActionTypes.ADD_COMMENTS,
+    payload: comments
+});
+
+export const fetchPromotions = () => dispatch => {
+    dispatch(promotionsLoading());
+
+    return fetch(baseUrl + 'promotions')
+        .then(response => response.json())
+        .then(promotions => dispatch(addPromotions(promotions)));
+};
+
+export const promotionsLoading = () => ({
+    type: ActionTypes.PROMOTIONS_LOADING
+});
+
+export const promotionsFailed = errMess => ({
+    type: ActionTypes.PROMOTIONS_FAILED,
+    payload: errMess
+});
+
+export const addPromotions = promotions => ({
+    type: ActionTypes.ADD_PROMOTIONS,
+    payload: promotions
 });
--- src/redux/commentsReducer.js
+++ src/redux/commentsReducer.js
@@ -1,15 +1,22 @@
 
-import { COMMENTS } from '../shared/comments';   
 import * as ActionTypes from './ActionTypes';
 
 /* this is the reducer function to handle this part of the state */
-export const CommentsReducer = (state = COMMENTS, action) => {
+export const CommentsReducer = (state = { errMess: null, comments: []}, action) => {
     switch (action.type) {
+        case ActionTypes.ADD_COMMENTS:
+            return {...state, errMess: null, comments: action.payload};
+
+        case ActionTypes.COMMENTS_FAILED:
+            return {...state, errMess: action.payload};
+
         case ActionTypes.ADD_COMMENT:
             const comment = action.payload;
-            comment.id = state.length;
+            comment.id = state.comments.length;
             comment.date = new Date().toISOString();
-            return state.concat(comment);
-        default: return state; 
-    }       
-}
+            return {...state, comments: state.comments.concat(comment)};
+
+        default:
+            return state;
+    }
+};
